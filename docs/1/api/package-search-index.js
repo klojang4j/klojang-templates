bdf555d9a012c0577aacc7f5f0423ade9122e9ac
Ayco Holleman <ayco.holleman@naturalis.nl>
2023-06-08 13:31:43 +0200

    version bump

---
+++ docs/1/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.klojang.templates","l":"org.klojang.templates"},{"m":"org.klojang.templates","l":"org.klojang.templates.name"}];updateSearchResults();
--- src/test/java/org/klojang/templates/EscapeTest.java
+++ src/test/java/org/klojang/templates/EscapeTest.java
@@ -3,6 +3,7 @@ package org.klojang.templates;
 import org.apache.http.client.utils.URIBuilder;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import static org.apache.commons.text.StringEscapeUtils.escapeEcmaScript;
@@ -23,5 +24,14 @@ public class EscapeTest {
     assertEquals("%25%5E%23%22%27%3D%5C%2F", ub0.toString().substring(2));
     assertEquals(escapeEcmaScript(ub0.toString().substring(2)), ub0.toString().substring(2));
     assertEquals(escapeHtml4(ub0.toString().substring(2)), ub0.toString().substring(2));
+  }
+
+  @Test
+  public void test01() throws ClassNotFoundException {
+    Class<?> c = Class.forName("org.klojang.check.Check");
+    Method[] mm = c.getDeclaredMethods();
+    for(Method m:mm) {
+      System.out.println("-> "+m.getName());
+    }
   }
 }
